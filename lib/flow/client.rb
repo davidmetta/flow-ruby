d0af1847305a02c3149e2388431dff6580a522ec
david metta <davideliemetta@gmail.com>
2021-03-13 02:42:36 -0500

    feat: multiple endpoints completed

--- lib/flow/client.rb
+++ lib/flow/client.rb
@@ -1,128 +1,223 @@
 # frozen_string_literal: true
 
 module Flow
+  #
+  # The client class is used to access the Flow API
+  #
   class Client
-    MAINNET_URL = "access.mainnet.nodes.onflow.org:9000"
-    TESTNEST_URL = "access.devnet.nodes.onflow.org:9000"
-    CANARYNET_URL = "access.canary.nodes.onflow.org:9000"
+    NODES = {
+      mainnet: "access.mainnet.nodes.onflow.org:9000",
+      testnet: "access.devnet.nodes.onflow.org:9000",
+      canarynet: "access.canary.nodes.onflow.org:9000"
+    }.freeze
 
-    def initialize(node_address = MAINNET_URL)
-      @stub = Access::AccessAPI::Stub.new(node_address, :this_channel_is_insecure)
+    #
+    # @param node [Symbol, String] the node address used in the stub
+    #
+    def initialize(node: :mainnet)
+      @stub = Access::AccessAPI::Stub.new(detect_node(node), :this_channel_is_insecure)
     end
 
+    #
+    # Check if the Access API is ready and available
+    #
+    # @return [Access::PingResponse]
+    #
     def ping
       @stub.ping(Access::PingRequest.new)
     end
 
-    #### Block Headers
-    ## The following methods query information about block headers.
+    ####
+    # Block Headers
+    #
+    # The following methods query information about block headers
+    ####
 
-    ## GetLatestBlockHeader
-    # GetLatestBlockHeader gets the latest sealed or unsealed block header.
-
-    def get_latest_block_header(is_sealed = false)
+    #
+    # Get the latest sealed or unsealed block header
+    #
+    # @param is_sealed [Boolean]
+    #
+    # @return [Flow::Entities::BlockHeader]
+    #
+    def get_latest_block_header(is_sealed: false)
       req = Access::GetLatestBlockHeaderRequest.new(is_sealed: is_sealed)
-      @stub.get_latest_block_header(req)
+      res = @stub.get_latest_block_header(req)
+      res.block
     end
 
-    ## GetBlockHeaderByID
-    # GetBlockHeaderByID gets a block header by ID.
-
+    #
+    # Get a block header by ID
+    #
+    # @todo The ID should be +bytes+ or it should be formatted automatically
+    #
+    # @param id [String]
+    #
+    # @return [Flow::Entities::BlockHeader]
+    #
     def get_block_header_by_id(id)
-      # to_bytes if its not bytes
       req = Access::GetBlockHeaderByIDRequest.new(id: id)
-      @stub.get_block_header_by_id(req)
+      res = @stub.get_block_header_by_id(req)
+      res.block
     end
 
-    ## GetBlockHeaderByHeight
-    # GetBlockHeaderByHeight gets a block header by height.
-
+    #
+    # Get a block header by height
+    #
+    # @param height [uint64]
+    #
+    # @return [Flow::Entities::BlockHeader]
+    #
     def get_block_header_by_height(height)
       req = Access::GetBlockHeaderByHeightRequest.new(height: height)
-      @stub.get_block_header_by_height(req)
+      res = @stub.get_block_header_by_height(req)
+      res.block
     end
 
-    #### Blocks
-    ## The following methods query information about full blocks.
+    ####
+    # Blocks
+    #
+    # The following methods query information about full blocks
+    ####
 
-    ## GetLatestBlock
-    # GetLatestBlock gets the full payload of the latest sealed or unsealed block.
-
-    def get_latest_block(is_sealed = false)
+    #
+    # Get the full payload of the latest sealed or unsealed block
+    #
+    # @param is_sealed [Boolean]
+    #
+    # @return [Flow::Entities::Block]
+    #
+    def get_latest_block(is_sealed: false)
       req = Access::GetLatestBlockRequest.new(is_sealed: is_sealed)
-      @stub.get_latest_block(req)
+      res = @stub.get_latest_block(req)
+      res.block
     end
 
-    ## GetBlockByID
-    # GetBlockByID gets a full block by ID.
-
+    #
+    # Get a full block by ID
+    #
+    # @todo The ID should be +bytes+ or it should be formatted automatically
+    #
+    # @param id [String]
+    #
+    # @return [Flow::Entities::Block]
+    #
     def get_block_by_id(id)
-      # to_bytes if its not bytes
       req = Access::GetBlockByIDRequest.new(id: id)
-      @stub.get_block_by_id(req)
+      res = @stub.get_block_by_id(req)
+      res.block
     end
 
-    ## GetBlockByHeight
-    # GetBlockByHeight gets a full block by height.
-
+    #
+    # Get a full block by height
+    #
+    # @param height [uint64]
+    #
+    # @return [Flow::Entities::Block]
+    #
     def get_block_by_height(height)
       req = Access::GetBlockByHeightRequest.new(height: height)
-      @stub.get_block_by_height(req)
+      res = @stub.get_block_by_height(req)
+      res.block
     end
 
-    #### Collections
-    ## The following methods query information about collections.
+    ####
+    # Collections
+    #
+    # The following methods query information about collections
+    ####
 
-    ## GetCollectionByID
-    # GetCollectionByID gets a collection by ID.
-
+    #
+    # Gets a collection by ID
+    #
+    # @todo The ID should be +bytes+ or it should be formatted automatically
+    #
+    # @param id [String]
+    #
+    # @return [Flow::Entities::Collection]
+    #
     def get_collection_by_id(id)
-      # to_bytes if its not bytes
       req = Access::GetCollectionByIDRequest.new(id: id)
-      @stub.get_collection_by_id(req)
+      res = @stub.get_collection_by_id(req)
+      res.collection
     end
 
-    #### Transactions
-    ## The following methods can be used to submit transactions and fetch their results.
+    ####
+    # Transactions
+    #
+    # The following methods can be used to submit transactions and fetch their results
+    ####
 
-    ## SendTransaction
-    # SendTransaction submits a transaction to the network.
+    #
+    # Submit a transaction to the network
+    #
+    #
+    #
+    #
+    # def send_transaction()
+    #   # TODO: build transaction here
+    #   # req = Access::SendTransactionRequest.new()
+    #   # @stub.send_transaction(req)
+    # end
 
-    def send_transaction()
-      # TODO: build transaction here
-      # req = Access::SendTransactionRequest.new()
-      # @stub.send_transaction(req)
-    end
+    #
+    # Get a transaction by ID
+    #
+    #
+    #
+    #
+    # def get_transaction(id)
+    #   # to_bytes if its not bytes
+    #   req = Access::GetTransactionRequest.new(id: id)
+    #   @stub.get_transaction(req)
+    # end
 
-    ## GetTransaction
-    # GetTransaction gets a transaction by ID.
+    #
+    # Get the execution result of a transaction
+    #
+    #
+    #
+    #
+    # def get_transaction_result(id)
+    #   # to_bytes if its not bytes
+    #   req = Access::GetTransactionRequest.new(id: id)
+    #   @stub.get_transaction_result(req)
+    # end
 
-    def get_transaction(id)
-      # to_bytes if its not bytes
-      req = Access::GetTransactionRequest.new(id: id)
-      @stub.get_transaction(req)
-    end
+    ####
+    # Accounts
+    ####
 
-    ## GetTransactionResult
-    # GetTransactionResult gets the execution result of a transaction.
-
-    def get_transaction_result(id)
-      # to_bytes if its not bytes
-      req = Access::GetTransactionRequest.new(id: id)
-      @stub.get_transaction_result(req)
-    end
-
-    #### Accounts
-
-    ## GetAccountAtLatestBlock
-    # GetAccountAtLatestBlock gets an account by address.
-
+    #
+    # Get an account by address
+    #
+    # @param address [String]
+    #
+    # @return [Flow::Entities::Account]
+    #
     def get_account_at_latest_block(address)
-      # to_bytes(address)
-      req = Access::GetAccountAtLatestBlockRequest.new(address: address)
-      @stub.get_account_at_latest_block(req)
-      # res.account
+      req = Access::GetAccountAtLatestBlockRequest.new(address: to_bytes(address))
+      res = @stub.get_account_at_latest_block(req)
+      res.account
     end
+
+    #
+    # Get an account by address at the given block height
+    #
+    # @param address [String]
+    # @param block_height [uint64]
+    #
+    # @return [Flow::Entities::Account]
+    #
+    def get_account_at_block_height(address, block_height)
+      req = Access::GetAccountAtBlockHeightRequest.new(address: to_bytes(address), block_height: block_height)
+      res = @stub.get_account_at_latest_block(req)
+      res.account
+    end
+
+    ####
+    # Scripts
+    ####
 
     # def execute_script(script, args = [])
     #   req = Access::ExecuteScriptAtLatestBlockRequest.new(script: script, arguments: args)
@@ -130,7 +225,56 @@ module Flow
     #   parse_json(res.value)
     # end
 
+    ####
+    # Events
+    #
+    # The following methods can be used to query for on-chain events
+    ####
+
+    #
+    # Retrieve events emitted within the specified block range
+    #
+    # @param type [String]
+    # @param start_height [uint64]
+    # @param end_height [uint64]
+    #
+    # @return []
+    #
+    # def get_events_for_height_range(type, start_height: 2, end_height: 3)
+    #   req = Access::GetEventsForHeightRangeRequest.new(type: type, start_height: start_height, end_height: end_height)
+    #   res = @stub.get_events_for_height_range(req)
+    #   res
+    # end
+
+    ####
+    # Network Parameters
+    #
+    # Network parameters provide information about the Flow network.
+    #   Currently, it only includes the chain ID.
+    #   The following method can be used to query for network parameters
+    ####
+
+    #
+    # Retrieve the network parameters
+    #
+    # @return [Hash]
+    #
+    def get_network_parameters
+      req = Access::GetNetworkParametersRequest.new
+      res = @stub.get_network_parameters(req)
+      res.to_h
+    end
+
     private
+
+    def detect_node(node)
+      case node.to_sym
+      when :mainnet   then NODES[:mainnet]
+      when :testnet   then NODES[:testnet]
+      when :canarynet then NODES[:canarynet]
+      else                 node
+      end
+    end
 
     def parse_json(event_payload)
       JSON.parse(event_payload, object_class: OpenStruct)
