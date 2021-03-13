d43270decda1da7d79f2f719e0f238dbbc829be0
david metta <davideliemetta@gmail.com>
2021-03-12 21:04:40 -0500

    feat: implemented client block headers, blocks, collections, transactions, and accounts

--- lib/flow/client.rb
+++ lib/flow/client.rb
@@ -14,11 +14,115 @@ module Flow
       @stub.ping(Access::PingRequest.new)
     end
 
-    # def get_account(address)
-    #   req = Access::GetAccountAtLatestBlockRequest.new(address: to_bytes(address))
-    #   res = @stub.get_account_at_latest_block(req)
-    #   res.account
-    # end
+    #### Block Headers
+    ## The following methods query information about block headers.
+
+    ## GetLatestBlockHeader
+    # GetLatestBlockHeader gets the latest sealed or unsealed block header.
+
+    def get_latest_block_header(is_sealed = false)
+      req = Access::GetLatestBlockHeaderRequest.new(is_sealed: is_sealed)
+      @stub.get_latest_block_header(req)
+    end
+
+    ## GetBlockHeaderByID
+    # GetBlockHeaderByID gets a block header by ID.
+
+    def get_block_header_by_id(id)
+      # to_bytes if its not bytes
+      req = Access::GetBlockHeaderByIDRequest.new(id: id)
+      @stub.get_block_header_by_id(req)
+    end
+
+    ## GetBlockHeaderByHeight
+    # GetBlockHeaderByHeight gets a block header by height.
+
+    def get_block_header_by_height(height)
+      req = Access::GetBlockHeaderByHeightRequest.new(height: height)
+      @stub.get_block_header_by_height(req)
+    end
+
+    #### Blocks
+    ## The following methods query information about full blocks.
+
+    ## GetLatestBlock
+    # GetLatestBlock gets the full payload of the latest sealed or unsealed block.
+
+    def get_latest_block(is_sealed = false)
+      req = Access::GetLatestBlockRequest.new(is_sealed: is_sealed)
+      @stub.get_latest_block(req)
+    end
+
+    ## GetBlockByID
+    # GetBlockByID gets a full block by ID.
+
+    def get_block_by_id(id)
+      # to_bytes if its not bytes
+      req = Access::GetBlockByIDRequest.new(id: id)
+      @stub.get_block_by_id(req)
+    end
+
+    ## GetBlockByHeight
+    # GetBlockByHeight gets a full block by height.
+
+    def get_block_by_height(height)
+      req = Access::GetBlockByHeightRequest.new(height: height)
+      @stub.get_block_by_height(req)
+    end
+
+    #### Collections
+    ## The following methods query information about collections.
+
+    ## GetCollectionByID
+    # GetCollectionByID gets a collection by ID.
+
+    def get_collection_by_id(id)
+      # to_bytes if its not bytes
+      req = Access::GetCollectionByIDRequest.new(id: id)
+      @stub.get_collection_by_id(req)
+    end
+
+    #### Transactions
+    ## The following methods can be used to submit transactions and fetch their results.
+
+    ## SendTransaction
+    # SendTransaction submits a transaction to the network.
+
+    def send_transaction()
+      # TODO: build transaction here
+      # req = Access::SendTransactionRequest.new()
+      # @stub.send_transaction(req)
+    end
+
+    ## GetTransaction
+    # GetTransaction gets a transaction by ID.
+
+    def get_transaction(id)
+      # to_bytes if its not bytes
+      req = Access::GetTransactionRequest.new(id: id)
+      @stub.get_transaction(req)
+    end
+
+    ## GetTransactionResult
+    # GetTransactionResult gets the execution result of a transaction.
+
+    def get_transaction_result(id)
+      # to_bytes if its not bytes
+      req = Access::GetTransactionRequest.new(id: id)
+      @stub.get_transaction_result(req)
+    end
+
+    #### Accounts
+
+    ## GetAccountAtLatestBlock
+    # GetAccountAtLatestBlock gets an account by address.
+
+    def get_account_at_latest_block(address)
+      # to_bytes(address)
+      req = Access::GetAccountAtLatestBlockRequest.new(address: address)
+      @stub.get_account_at_latest_block(req)
+      # res.account
+    end
 
     # def execute_script(script, args = [])
     #   req = Access::ExecuteScriptAtLatestBlockRequest.new(script: script, arguments: args)
